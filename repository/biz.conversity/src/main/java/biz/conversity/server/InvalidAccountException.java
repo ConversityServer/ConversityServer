8586dee068133463e42221ca30159543ae1c06ff
ConversityServer <43730531+ConversityServer@users.noreply.github.com>
2019-01-24 12:25:06 -0600

    Delete InvalidAccountException.java

--- repository/biz.conversity/src/main/java/biz/conversity/server/InvalidAccountException.java
+++
@@ -1,8 +0,0 @@
-package biz.conversity.server;
-
-public class InvalidAccountException extends RuntimeException{
-	static final long serialVersionUID = 1;
-	public InvalidAccountException(String message) {
-		super(message);
-	}
-}
